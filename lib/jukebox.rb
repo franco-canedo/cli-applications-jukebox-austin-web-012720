26369b60fc97bb0a83a2c832a406ef3375c4cf9b
Franco Canedo <francanedo1@hotmail.com>
2020-01-26 22:24:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,14 +22,7 @@ end
 def play(array)
   puts "Please enter a song name or number:"
   input = gets.strip
-  array.each {|song|
-    if input == array[0] 
-      puts "Playing #{array[input - 1]}"
-    end 
-  }
-  if input.to_f == 1...9
-    puts "Playing #{array[input - 1]}"
-  end 
+  
 end 
 
 def list(array)
